180addb1a7294f297501c37e5be4869cff26f562
yoelleghesse <yoelleghesse@gmail.com>
2024-12-10 20:24:20 -0500

    block types and tests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -9,3 +9,22 @@ def markdown_to_blocks(markdown):
     for mk in markdown:
         filtered.append(mk.strip())
     return filtered
+
+
+def block_to_block_type(block):
+    block = block.strip()
+    if block.startswith("* "):
+        return "unordered_list"
+    if block[0].isdigit() and block[1] == ".":
+        return "ordered_list"
+    if block.startswith("#"):
+        return "heading"
+    if block.startswith(">"):
+        return "quote"
+    if block.startswith("```"):
+        return "code"
+    else:
+        return "paragraph"
+    
+    
+
--- src/test_markdown_block.py
+++ src/test_markdown_block.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type
 
 class TestMarkdownToHTML(unittest.TestCase):
 
@@ -22,6 +22,23 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+
+
+    def test_block_to_block_type_heading(self):
+        self.assertEqual(block_to_block_type("# Heading"), "heading")
+
+    def test_block_to_block_type_ordered_list(self):
+        self.assertEqual(block_to_block_type("1. First item\n2. Second item"), "ordered_list")
+
+    def test_block_to_block_type_unordered_list(self):
+        self.assertEqual(block_to_block_type("* First item\n* Second item"), "unordered_list")
+
+    def test_block_to_block_type_quote(self):
+        self.assertEqual(block_to_block_type("> This is a quote"), "quote")
+    
+    def test_block_to_block_type_code(self):
+        self.assertEqual(block_to_block_type("```\nThis is a code block\n```"), "code")
+        
 
 if __name__ == "__main__":
     unittest.main()
