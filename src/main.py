fe528598fff52ea7436a5aff9dc9cc028c32c5af
yoelleghesse <yoelleghesse@gmail.com>
2024-12-11 21:23:57 -0500

    generate page add

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import os
 import shutil
-
 from copystatic import copy_files_recursive
+from generatepage import generate_page
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+content_path = "./content/index.md"
+template_path = "./template.html"
+output_path = "./public/index.html"
 
 def main():
     print("deleting public directory")
@@ -13,5 +16,8 @@ def main():
 
     print("Copying static files to public directory")
     copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating HTML page")
+    generate_page(content_path, template_path, output_path)    
     
 main()
