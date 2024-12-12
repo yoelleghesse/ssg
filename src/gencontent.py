cfa6fc98faeb4dbd4f7efeac1b996c5881d9a7ff
yoelleghesse <yoelleghesse@gmail.com>
2024-12-11 23:21:26 -0500

    gen pages recursive add

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 
@@ -32,3 +33,13 @@ def extract_title(md):
         if line.startswith("# "):
             return line[2:]
     raise ValueError("No title found")
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
